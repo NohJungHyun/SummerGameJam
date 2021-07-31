f7850a2acfb8089163ccf55bcecacc57bd612a95
JungHyun <adad5151@naver.com>
2021-07-31 19:08:05 +0900

    변경사항
    작습니다

--- Assets/ScriptableObjects/MonsterRevealTable.cs
+++ Assets/ScriptableObjects/MonsterRevealTable.cs
@@ -2,17 +2,59 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class MonsterRevealTable : MonoBehaviour
+public class GhostEncountPercent
 {
-    // Start is called before the first frame update
-    void Start()
+    public Ghost ghost;
+    public float encountPercentage;
+    float pay;
+
+    public void SetPay(float p)
     {
-        
+        pay = p;
     }
 
-    // Update is called once per frame
-    void Update()
+    public float GetPay()
     {
-        
+        return pay;
+    }
+}
+
+public class MonsterRevealTable : ScriptableObject
+{
+
+    public List<GhostEncountPercent> monsterRevealList = new List<GhostEncountPercent>();
+    // Start is called before the first frame update
+    List<GhostEncountPercent> tempList;
+
+    public Ghost RecruitMonster()
+    {
+        float totalPercent = 0;
+        Ghost selectedOne = null;
+
+        for (int reveal = 0; reveal < monsterRevealList.Count; reveal++)
+        {
+            totalPercent += monsterRevealList[reveal].encountPercentage;
+            monsterRevealList[reveal].SetPay(monsterRevealList[reveal].encountPercentage / totalPercent);
+        }
+
+        float randTicket = Random.Range(0, 1);
+
+        for (int i = 0; i < monsterRevealList.Count; i++)
+        {
+            if (monsterRevealList[i].GetPay() > randTicket)
+            {
+                selectedOne = monsterRevealList[i].ghost;
+                break;
+            }
+            else if (monsterRevealList[i].GetPay() < randTicket)
+                continue;
+
+            else if (monsterRevealList[monsterRevealList.Count - 1].GetPay() < randTicket)
+            {
+                selectedOne = monsterRevealList[monsterRevealList.Count - 1].ghost;
+                break;
+            }
+        }
+        return selectedOne;
     }
 }
